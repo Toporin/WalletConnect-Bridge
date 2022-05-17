d9e72e3c82699eff3cb3bad6cba6671afd771d6c
Toporin <satochip.wallet@gmail.com>
2022-05-17 21:47:35 +0100

    Updates

---
+++ precache-manifest.a30b85adad05587f866d0bcafd2eea14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e625286442ccaf6f487c",
+    "url": "/WalletConnect-Bridge/static/js/main.e6252864.chunk.js"
+  },
+  {
+    "revision": "e124be08e8196216a187",
+    "url": "/WalletConnect-Bridge/static/js/1.e124be08.chunk.js"
+  },
+  {
+    "revision": "20f2903209de700942c6",
+    "url": "/WalletConnect-Bridge/static/js/runtime~main.20f29032.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/WalletConnect-Bridge/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "91c7b6850731466bb0b57f98a95f9fbf",
+    "url": "/WalletConnect-Bridge/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/WalletConnect-Bridge/workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/WalletConnect-Bridge/workbox-v3.6.3"});
+
+importScripts(
+  "/WalletConnect-Bridge/precache-manifest.a30b85adad05587f866d0bcafd2eea14.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/WalletConnect-Bridge/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
